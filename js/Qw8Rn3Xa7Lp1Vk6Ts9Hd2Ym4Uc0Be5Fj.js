a7339e1592746f9839854d978300e5d5788304af
1bbnuuu <ibnuzzy29@gmail.com>
2026-05-18 13:56:41 +0700

    last push

--- js/Qw8Rn3Xa7Lp1Vk6Ts9Hd2Ym4Uc0Be5Fj.js
+++ js/Qw8Rn3Xa7Lp1Vk6Ts9Hd2Ym4Uc0Be5Fj.js
@@ -35,21 +35,21 @@ function renderRows(rows) {
         wrap.style.animationDelay = `${i * 40}ms`;
 
         wrap.innerHTML = `
-        <div class="folder relative group mt-5 w-44 h-[170px] cursor-pointer"
+        <div class="folder relative group mt-5 md:w-44 md:h-[170px] w-28 h-20 cursor-pointer"
              onclick="window.location.href='detail.html?id=${encodeURIComponent(profile.id)}'">
 
-            <svg class="absolute -z-40 w-44 text-cyan-600" viewBox="0 0 463 142" fill="none">
+            <svg class="absolute -z-40 md:w-44 text-cyan-600 w-28 md:top-0 top-3" viewBox="0 0 463 142" fill="none">
                 <path d="M36.792 0C16.4723 0 0 16.4723 0 36.792L0 141.281H463V73.9973C463 53.4494 446.343 36.792 425.795 36.792H214.137L201.224 35.8936C194.135 35.4005 187.335 32.8873 181.629 28.6515L156.202 9.7758C147.651 3.42757 137.284 0 126.633 0H36.792Z" fill="currentColor"/>
             </svg>
 
             <div class="absolute top-4 flex justify-center w-full">
-                <div class="bg-white w-5/6 h-32 group-hover:-translate-y-4 transition-transform duration-300 px-2 overflow-hidden">
+                <div class="bg-white w-5/6 md:h-32 h-20 group-hover:-translate-y-4 transition-transform duration-300 px-2 overflow-hidden">
                     <p class="font-bold text-sm">${profile.name}</p>
                     <p class="text-[8px]">${profile.note}</p>
                 </div>
             </div>
 
-            <svg class="absolute top-9 w-44 text-cyan-500 shadow-[0_-8px_15px_rgba(0,0,0,0.3)]" viewBox="0 0 463 323" fill="none">
+            <svg class="absolute top-9 md:w-44 w-28 text-cyan-500 shadow-[0_-8px_15px_rgba(0,0,0,0.3)]" viewBox="0 0 463 323" fill="none">
                 <rect width="463" height="323" rx="35" fill="currentColor"/>
             </svg>
 
